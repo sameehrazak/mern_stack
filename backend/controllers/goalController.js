a3efa567776fc4c38cd05d528dcd834b4a2fb239
S A M E E H R A Z A K <97714579+sameehrazak@users.noreply.github.com>
2023-07-05 01:39:25 +0530

    ready to deploy

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -39,16 +39,16 @@ const updateGoal = asyncHandler(async (req,res) => {
         throw new Error('Goal Not Found')
     }
 
-    const user = await User.findById(req.user.id)
+    // const user = await User.findById(req.user.id)
 
     //Check for user
-    if(!user) {
+    if(!req.user) {
         res.status(401)
         throw new Error('User not found')
     }
 
     //Make sure the logged in user matches the goal user
-    if(goal.user.toString() !== user.id) {
+    if(goal.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User not authorized')
     }
@@ -70,16 +70,16 @@ const deleteGoal = asyncHandler(async (req,res) => {
       res.status(400)
       throw new Error('Goal not found')
     }
-    const user = await User.findById(req.user.id)
+    // const user = await User.findById(req.user.id)
 
     //Check for user
-    if(!user) {
+    if(!req.user) {
         res.status(401)
         throw new Error('User not found')
     }
 
     //Make sure the logged in user matches the goal user
-    if(goal.user.toString() !== user.id) {
+    if(goal.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User not authorized')
     }
@@ -94,5 +94,4 @@ module.exports = {
     setGoal,
     updateGoal,
     deleteGoal,
-
 }
